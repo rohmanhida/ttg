94d1e0835044984d0879149fde14b18605ca6ca2
rohmanhida <hodyjones99@gmail.com>
2026-04-25 11:40:31 +0700

    leetcode 2

--- number4.py
+++ number4.py
@@ -1,1 +1,15 @@
+def findTergetSum(nums: list[int], target: int) -> int:
+    def recursive(i, total):
+        if i == len(nums):
+            return total == target
+        return (
+            recursive(i + 1, total + nums[i])
+            + recursive(i + 1, total - nums[i])
+            + recursive(i + 1, total * nums[i])
+        )
 
+    return recursive(0, 0)
+
+
+a = findTergetSum([2,3,5,10], 25)
+print(a)
