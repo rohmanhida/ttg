b33f7b0d991dfc1fe8cbf5511fca8d9af119b667
rohmanhida <hodyjones99@gmail.com>
2026-04-25 10:49:24 +0700

    cover for multiple number in leetcode 1

--- number3.py
+++ number3.py
@@ -6,11 +6,11 @@ def findMissingElement(arr: list[int]):
             result.append(arr[i]+1)
         if arr[i]-1 not in arr and arr[i]-1 not in result:
             result.append(arr[i]-1)
-        if len(result) == 3:
-            break
+
     result.sort()
-    return result[1]
+    # print('all result', result)
+    return result[1:-1:]
 
 
-a = findMissingElement([3106, 3102, 3104, 3105, 3107, 3103, 3109])
+a = findMissingElement([3106, 3102, 3105, 3107, 3103, 3109])
 print('result', a)
